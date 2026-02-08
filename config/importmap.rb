60f933012ab1f44ef8e3d60fb43cf3faa91ae745
Santiago Calvo <scalvo@eagerworks.com>
2026-02-08 11:23:43 +0000

    update gitignore and add config setup

---
+++ config/importmap.rb
@@ -0,0 +1,2 @@
+pin "d3", to: "https://cdn.jsdelivr.net/npm/d3@7/+esm", preload: false
+pin "trackplot", to: "trackplot/index.js", preload: true
---
+++ lib/trackplot/engine.rb
@@ -0,0 +1,22 @@
+module Trackplot
+  class Engine < ::Rails::Engine
+    isolate_namespace Trackplot
+
+    initializer "trackplot.helpers" do
+      ActiveSupport.on_load(:action_view) do
+        include Trackplot::ChartHelper
+      end
+    end
+
+    initializer "trackplot.assets" do |app|
+      app.config.assets.paths << root.join("app/assets/javascripts") if app.config.respond_to?(:assets)
+    end
+
+    initializer "trackplot.importmap", before: "importmap" do |app|
+      if app.config.respond_to?(:importmap)
+        app.config.importmap.paths << root.join("config/importmap.rb")
+        app.config.importmap.cache_sweepers << root.join("app/assets/javascripts")
+      end
+    end
+  end
+end
